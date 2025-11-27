ec081f1cfb2c5011bc394e08045d478a9a883bf2
kamran <kamranraz28@gmail.com>
2025-11-27 16:32:57 +0600

    returns migration file added

---
+++ database/migrations/2025_11_27_102829_create_returns_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('returns', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('dealer_id')
+                ->constrained('users')
+                ->onDelete('cascade');
+
+            $table->foreignId('retailer_id')
+                ->constrained('users')
+                ->onDelete('cascade');
+
+            $table->foreignId('stock_id')
+                ->constrained('stocks')
+                ->onDelete('cascade');
+
+            $table->tinyInteger('type');
+            $table->tinyInteger('status')->default(0);
+
+            $table->foreignId('approve_by')
+                ->nullable()
+                ->constrained('users')
+                ->onDelete('cascade');
+
+            $table->string('remarks')->nullable();
+
+            $table->tinyInteger('status')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('returns');
+    }
+};
